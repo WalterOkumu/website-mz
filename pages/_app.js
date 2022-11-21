1ba3aad89160096bda310f3ec65bfbbbd7890c27
Helder Fonseca <helder2391@gmail.com>
2022-11-21 09:52:28 -0100

    change text form and color

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
           <>
             <Head>
-              <title>Email Marketing</title>
+              <title>Website - Cabo Verde</title>
             </Head>
             <Component {...pageProps} />
           </>
